b2c9713c29d694d304ba181a807576a6d9e3362a
Wenxuan He <wenxh15@vt.edu>
2019-08-07 18:20:16 -0700

    Updates

---
+++ precache-manifest.338f805d1e2d850780769aa4efd0bbac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96a6a970aa763c424b8a06e16775af96",
+    "url": "/my-test-app/index.html"
+  },
+  {
+    "revision": "e8b8ecaa9cecbb233070",
+    "url": "/my-test-app/static/css/main.b792a898.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/my-test-app/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "e8b8ecaa9cecbb233070",
+    "url": "/my-test-app/static/js/main.9dc064fa.chunk.js"
+  },
+  {
+    "revision": "e811fc2b0fbf0742924e",
+    "url": "/my-test-app/static/js/runtime~main.ce8670d9.js"
+  }
+]);
